7bdf0ae686610f2ec9cc2daa938ef37d56b323f6
wuxin666789 <1356404098@qq.com>
2023-05-18 17:30:44 +0800

    Update http-client.tsx

--- web/src/services/api/http-client.tsx
+++ web/src/services/api/http-client.tsx
@@ -1,21 +1,21 @@
-import { AppSettings } from "../../appsettings"
+import { AppSettings } from "../../appsettings";
 
 export async function Post<T>(url: string, data?: object) {
-  return base<T>(url, "post", data)
+  return base<T>(url, "post", data);
 }
 export async function Get<T>(url: string) {
-  return base<T>(url, "get")
+  return base<T>(url, "get");
 }
 export interface SmartFaqResponse<T> {
-  code: number
-  msg: string
-  data: T
+  code: number;
+  msg: string;
+  data: T;
 }
 
 export interface IResponse<T> {
-  code: 200
-  msg: string
-  data: T
+  code: 200;
+  msg: string;
+  data: T;
 }
 
 export async function base<T>(
@@ -23,7 +23,7 @@ export async function base<T>(
   method: "get" | "post",
   data?: object | FormData
 ) {
-  const settings: AppSettings = (window as any).appSettings
+  const settings: AppSettings = (window as any).appSettings;
 
   const headers: { Authorization: string; "Content-Type"?: string } = {
     Authorization:
@@ -31,21 +31,29 @@ export async function base<T>(
       (localStorage.getItem("token")
         ? (localStorage.getItem("token") as string)
         : ""),
-  }
-  const isFormData = data instanceof FormData
-  if (!isFormData) headers["Content-Type"] = "application/json"
-  const body = isFormData ? data : JSON.stringify(data)
+  };
+  const isFormData = data instanceof FormData;
+  if (!isFormData) headers["Content-Type"] = "application/json";
+  const body = isFormData ? data : JSON.stringify(data);
   return await fetch(`${settings.serverUrl}${url}`, {
     method: method,
     body: body,
     headers: headers,
   })
-    .then((res) => res.json())
+    .then((res) => {
+      if (res.status === 401) {
+        localStorage.setItem("token", "");
+
+        return;
+      }
+
+      return res.json();
+    })
     .then((res: IResponse<T>) => {
       if (res.code === 200) {
-        return res.data
+        return res.data;
       } else {
-        throw new Error("request error")
+        throw new Error("request error");
       }
-    })
+    });
 }
