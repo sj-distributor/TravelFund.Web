bfbda0a4aaca0bc64dea73476273a98c09a7fbc0
KOKI.K <koki.kong@sjfood.com>
2023-09-08 17:10:14 +0800

    Update index.tsx

--- web/src/router/index.tsx
+++ web/src/router/index.tsx
@@ -47,7 +47,7 @@ export const Router = () => {
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route element={<Home />}>
-        <Route path="" element={<Navigate to={"/home/reimbursement"} />} />
+        <Route path="*" element={<Navigate to={"/home/reimbursement"} />} />
         {routerArray.map((item: RoutesProps, index: number) => {
           return (
             <Route
